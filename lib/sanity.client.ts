fdb1c35522261b2feb3646f32622d3e48c7551ba
Keng Panyawat <kengpanyawat@gmail.com>
2025-09-30 01:37:48 +0700

    build ok

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,16 +1,7 @@
-// lib/sanity.client.ts
-import {createClient} from 'next-sanity';
+import {createClient} from "@sanity/client";
 
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-06-01';
-
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,        // ← ใส่เวอร์ชันให้ชัด
-  useCdn: true,      // published content
-  perspective: 'published',
+export const sanityClient = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset:   process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  useCdn: true, // production ให้เร็ว
 });
